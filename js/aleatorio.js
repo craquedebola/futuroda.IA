e2d298ce348ef6e25eff211e95a1774a5c903e8f
joaocru <106744874+craquedebola@users.noreply.github.com>
2024-08-26 09:09:36 -0300

    myk

--- js/aleatorio.js
+++
@@ -1,9 +0,0 @@
-{const nomes = ["João Victor"];
-    
-    export function aleatorio (lista){
-        const posicao = Math.floor(Math.random()* lista.length);
-        return lista[posicao];
-    }
-    
-    export const nome = aleatorio(nomes)
-}
